ca8032458c3d3053bc71510ea9176b50284a1cf1
Thomas Weinert <thomas@weinert.info>
2018-05-16 10:53:50 +0200

    Test SearchApi with invalid configuration

--- tests/Api/SearchTest.php
+++ tests/Api/SearchTest.php
@@ -12,7 +12,20 @@ class SearchTest extends \PapayaTestCase {
     $result = $document->appendElement('test');
     $result->append($search->fetch(''));
     $this->assertXmlStringEqualsXmlString(
-      '<test><error identifier="EmptyQuery" severity="warning"/></test>',
+      '<test><message identifier="EmptyQuery" severity="warning"/></test>',
+      $result->saveXml()
+    );
+  }
+
+  public function testInvalidConfigurationExpectingError() {
+    $search = new Search(
+      '', '', ''
+    );
+    $document = new \PapayaXmlDocument();
+    $result = $document->appendElement('test');
+    $result->append($search->fetch('foo'));
+    $this->assertXmlStringEqualsXmlString(
+      '<test><message identifier="TechnicalError" severity="warning"/></test>',
       $result->saveXml()
     );
   }
